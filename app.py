9d52ccd19304fc2c411424aaede02433277eedc5
Andres Pineda <apiro99@outlook.com>
2022-05-03 23:42:54 -0600

    new Endpoints

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from users.Users import usersModule
+# from files.uploadfile import fileModule
 
+UPLOAD_FOLDER = './data/'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = 'super secret key'
 app.register_blueprint(usersModule)
+# app.register_blueprint(fileModule)
+
+@app.route('/file', methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    # check if the post request has the file part
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file:
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return { 'data': 'File uploaded succesfully' }, 200
+      # return redirect(url_for('download_file', name=filename))
+  return ''''''
+
--- users/Users.py
+++ users/Users.py
@@ -3,8 +3,6 @@ from flask import Flask, request, Blueprint
 
 users = [];
 
-# app = Flask(__name__)
-
 usersModule = Blueprint('simple_page', __name__, template_folder='templates')
 
 @usersModule.route('/users', methods=['GET'])
@@ -12,6 +10,13 @@ def getUsers():
   return {
     'data': list(map(lambda x: x['name'], users))
   }, 200
+
+@usersModule.route('/users/<name>', methods=['GET'])
+def getUser(name):
+  for user in users:
+    if user['name'] == name:
+      return { 'data': user["rates"] }, 200;
+  return { 'data': 'User not found' }, 404;
 
 
 @usersModule.route('/users/signin', methods=['POST'])
@@ -23,25 +28,40 @@ def signin():
     'rates': [],
     'model': {}
   };
+  for user in users: 
+    if user['name'] == data['username']: return { 'data': 'User Already exists' }, 409;
   users.append(newUser);
-  print(users);
   return { 'data': 'User register correctly' }, 201;
 
 
 @usersModule.route('/users/login/<name>', methods=['POST'])
 def login(name):
   for user in users:
-    if user['name'] == name: return { 'data': 'User found' }, 200;
-  return { 'data': 'User doesnt exists' }, 404;
+    if user['name'] == name: return { 'data': True }, 200;
+  return { 'data': False }, 404;
 
 
 @usersModule.route('/users/addReview/<name>', methods=['POST'])
 def addReview(name):
   for user in users:
     if user['name'] == name:
+      for rate in user['rates']: 
+        if request.json["movie"] == rate["movie"]: return { 'data': 'review already exists' }, 409;
+      
       # Train model according to user review
       user['rates'].append(request.json);
       return { 'data': 'Review added' }, 200;
+  return { 'data': 'User doesnt exists' }, 404;
+
+@usersModule.route('/users/updateReview/<name>', methods=['PUT'])
+def updateReview(name):
+  for user in users:
+    if user['name'] == name:
+      for rate in user['rates']: 
+        if request.json["movie"] == rate["movie"]: 
+          rate["liked"] = request.json["liked"];
+          return { 'data': 'Review updated' }, 200;
+      return { 'data': 'Review doesnt exists' }, 404;
   return { 'data': 'User doesnt exists' }, 404;
 
 
@@ -49,6 +69,6 @@ def addReview(name):
 def getRecomendation(name):
   for user in users:
     if user['name'] == name:
-      recomendations = user['model'].getRecomendation();
-      return { 'data': recomendations }, 200;
+      # recomendations = user['model'].getRecomendation();
+      return { 'data': "Waifus :)" }, 200;
   return { 'data': 'User not found' }, 404;
